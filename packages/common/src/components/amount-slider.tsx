213cb794b4180fc21be952720361acee6c19201e
Ferdinand Martini <ferdinand.martini@web.de>
2025-10-07 17:55:46 +0200

    amount slider balance to fixed

--- packages/common/src/components/amount-slider.tsx
+++ packages/common/src/components/amount-slider.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import * as SliderPrimitive from '@radix-ui/react-slider';
 
-import { cn } from '../lib/utils';
-import { formatUnits } from 'viem';
+import { cn, formatUnits } from '../lib/utils';
 
 export type AmountSliderProps = {
     amount: string;
